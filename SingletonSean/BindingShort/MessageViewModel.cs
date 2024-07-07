8f2d36757e4a1100059dab9c269f5a8af1a4dd12
tricdt <tricdt@gmail.com>
2024-07-07 10:36:24 +0700

    Binding Short

---
+++ SingletonSean/BindingShort/MessageViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BindingShort
+{
+    public class MessageViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+		private string _message;
+
+		public string Message
+		{
+			get { return _message; }
+			set { 
+				_message = value; 
+				OnPropertyChanged(nameof(Message)); 
+			}
+		}
+		protected void OnPropertyChanged(string propertyName)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
+}
