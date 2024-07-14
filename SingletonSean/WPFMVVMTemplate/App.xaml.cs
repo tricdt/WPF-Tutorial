039be52d98e370953eaa01baed798f37ca87aff5
tricdt <tricdt@gmail.com>
2024-07-14 13:59:06 +0700

    [SwitchingViewMVVM] Init

---
+++ SingletonSean/WPFMVVMTemplate/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WPFMVVMTemplate
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
