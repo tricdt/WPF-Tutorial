9dd98ffae3fb1df4539a9a6cfc1d195f8364591f
tricdt <tricdt@gmail.com>
2024-07-15 14:11:31 +0700

    [AsyncCommands] IsExcuting

--- SingletonSean/AsyncCommands/Commands/AsyncCommandBase.cs
+++ SingletonSean/AsyncCommands/Commands/AsyncCommandBase.cs
@@ -4,16 +4,30 @@ namespace AsyncCommands.Commands
 {
     public abstract class AsyncCommandBase : ICommand
     {
+        private bool _isExcuting;
+
+        public bool IsExcuting
+        {
+            get { return _isExcuting; }
+            set
+            {
+                _isExcuting = value;
+                CanExecuteChanged.Invoke(this, new EventArgs());
+            }
+        }
+
         public event EventHandler CanExecuteChanged;
 
         public bool CanExecute(object parameter)
         {
-            return true;
+            return !IsExcuting;
         }
 
         public async void Execute(object parameter)
         {
+            IsExcuting = true;
             await ExcuteAsync(parameter);
+            IsExcuting = false;
         }
 
         protected abstract Task ExcuteAsync(object parameter);
