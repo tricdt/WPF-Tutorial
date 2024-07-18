56cef98899b0f4d26a1e81e0c0c4be2d35ee5c48
tricdt <tricdt@gmail.com>
2024-07-18 10:07:18 +0700

    [LayoutComponents] add PageLayout component

---
+++ SingletonSean/LayoutComponents/Components/PageLayout.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace LayoutComponents.Components
+{
+    /// <summary>
+    /// Interaction logic for PageLayout.xaml
+    /// </summary>
+    public partial class PageLayout : UserControl
+    {
+
+
+        public string Header
+        {
+            get { return (string)GetValue(HeaderProperty); }
+            set { SetValue(HeaderProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Header.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty HeaderProperty =
+            DependencyProperty.Register("Header", typeof(string), typeof(PageLayout), new PropertyMetadata(string.Empty));
+
+
+        public PageLayout()
+        {
+            InitializeComponent();
+        }
+    }
+}
