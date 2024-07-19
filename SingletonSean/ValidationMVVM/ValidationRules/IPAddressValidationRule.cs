4ab4e8809189e5ef4662fd0a8c31ea6f5e67e014
tricdt <tricdt@gmail.com>
2024-07-19 14:31:09 +0700

    [ValidationMVVM] add IP address validation rule

---
+++ SingletonSean/ValidationMVVM/ValidationRules/IPAddressValidationRule.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+using System.Net;
+using System.Windows.Controls;
+
+namespace ValidationMVVM.ValidationRules
+{
+    public class IPAddressValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (IPAddress.TryParse(value.ToString(), out IPAddress ipAddress))
+            {
+                return ValidationResult.ValidResult;
+            }
+
+            return new ValidationResult(false, "IP address is invalid.");
+        }
+    }
+}
