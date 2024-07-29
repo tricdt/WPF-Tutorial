e12ab0716f701346248bfe8088746e48301a0f57
tricdt <tricdt@gmail.com>
2024-07-29 20:12:41 +0700

    [ItemsControlDemo] init solution

---
+++ SingletonSean/ItemsControlDemo/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using ItemsControlDemo.ViewModels;
+using System.Windows;
+namespace ItemsControlDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainViewModel();
+        }
+    }
+}
---
+++ SingletonSean/ItemsControlDemo/ViewModels/MainViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace ItemsControlDemo.ViewModels
+{
+    public class MainViewModel
+    {
+    }
+}
