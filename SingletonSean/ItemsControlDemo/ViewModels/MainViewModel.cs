8fa3bb3f416946bb62fc54dddabc9fa8dfa2a2b5
tricdt <tricdt@gmail.com>
2024-07-29 20:17:27 +0700

    [ItemsControlDemo] Add ItemsControl demo

--- SingletonSean/ItemsControlDemo/ViewModels/MainViewModel.cs
+++ SingletonSean/ItemsControlDemo/ViewModels/MainViewModel.cs
@@ -1,6 +1,21 @@
-﻿namespace ItemsControlDemo.ViewModels
+﻿using MVVMEssentials.ViewModels;
+using System.Collections.ObjectModel;
+namespace ItemsControlDemo.ViewModels
 {
-    public class MainViewModel
+    public class MainViewModel : ViewModelBase
     {
+        private readonly ObservableCollection<ProductViewModel> _productViewModels;
+        public IEnumerable<ProductViewModel> ProductViewModels { get { return _productViewModels; } }
+        public MainViewModel()
+        {
+            _productViewModels = new ObservableCollection<ProductViewModel>()
+            {
+                new ProductViewModel("T-Shirt", "A beautiful blue t-shirt.", 29.99),
+                new ProductViewModel("Sweatpants", "The greatest gray sweatpants.", 19.99),
+                new ProductViewModel("Shoes", "Some radical red shoes.", 179.99),
+                new ProductViewModel("Socks", "Just regular socks.", 4.99),
+                new ProductViewModel("Shorts", "A pair of shorts that you can use to wear around in, play sports, or record YouTube videos.", 19.99)
+            };
+        }
     }
 }
