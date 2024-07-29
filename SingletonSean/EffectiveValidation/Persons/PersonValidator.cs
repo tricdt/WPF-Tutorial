a8f662868ca830259d4e7acc5a2139f8ec144944
tricdt <tricdt@gmail.com>
2024-07-29 15:17:54 +0700

    [EffectiveValidation] Add person interface demo

---
+++ SingletonSean/EffectiveValidation/Persons/PersonValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+namespace EffectiveValidation.Persons
+{
+    public class PersonValidator : AbstractValidator<IPerson>
+    {
+        public PersonValidator()
+        {
+            RuleFor(p => p.FullName)
+                .MinimumLength(3)
+                .MaximumLength(50);
+        }
+    }
+}
