2d4b4b315ca4e7db5834b6a6dc34eadd8c9b8daf
tricdt <tricdt@gmail.com>
2024-08-04 08:12:48 +0700

    [Reservoom] add DTOs and Implementing the DBContext

---
+++ SingletonSean/Reservoom/DbContexts/ReservoomDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Reservoom.DTOs;
+namespace Reservoom.DbContexts
+{
+    public class ReservoomDbContext : DbContext
+    {
+        public DbSet<ReservationDTO> Reservations { get; set; }
+    }
+}
