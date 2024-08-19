d1ff1131e392eb071ddda49b305072ff24a932c2
tricdt <tricdt@gmail.com>
2024-08-19 14:03:53 +0700

    [SimpleTrader] add dependency injection

--- SingletonSean/SimpleTrader.WPF/App.xaml.cs
+++ SingletonSean/SimpleTrader.WPF/App.xaml.cs
@@ -1,8 +1,10 @@
-﻿using SimpleTrader.Domain.Models;
+﻿using Microsoft.Extensions.DependencyInjection;
+using SimpleTrader.Domain.Models;
 using SimpleTrader.Domain.Services;
 using SimpleTrader.Domain.Services.TransactionServices;
 using SimpleTrader.EntityFramework.Services;
 using SimpleTrader.FinancialModelingPrepAPI.Services;
+using SimpleTrader.WPF.State.Navigators;
 using SimpleTrader.WPF.ViewModels;
 using System.Windows;
 namespace SimpleTrader.WPF
@@ -16,17 +18,31 @@ namespace SimpleTrader.WPF
         string baseUrl = "https://financialmodelingprep.com/api/v3/";
         protected override async void OnStartup(StartupEventArgs e)
         {
+            IServiceProvider serviceProvider = CreateServiceProvider();
             IDataService<Account> accountService = new AccountDataService(new EntityFramework.SimpleTraderDbContextFactory());
             IStockPriceService stockPriceService = new StockPriceService(new FinancialModelingPrepAPI.FinancialModelingPrepHttpClient(new System.Net.Http.HttpClient() { BaseAddress = new Uri(baseUrl) }, new FinancialModelingPrepAPI.Models.FinancialModelingPrepAPIKey(_api_key)));
             IBuyStockService buyStockService = new BuyStockService(stockPriceService, accountService);
             Account buyer = await accountService.Get(1);
             buyStockService.BuyStock(buyer, "AAPL", 1);
-            MainWindow = new MainWindow()
-            {
-                DataContext = new MainViewModel()
-            };
-            MainWindow.Show();
+            Window window = serviceProvider.GetService<MainWindow>();
+            window.Show();
+            //MainWindow = new MainWindow()
+            //{
+            //    DataContext = new MainViewModel(new Navigator())
+            //};
+            //MainWindow.Show();
             base.OnStartup(e);
+        }
+        private IServiceProvider CreateServiceProvider()
+        {
+            IServiceCollection services = new ServiceCollection();
+            services.AddScoped<INavigator, Navigator>();
+            services.AddScoped<MainViewModel>();
+            services.AddScoped<MainWindow>(s => new MainWindow()
+            {
+                DataContext = s.GetRequiredService<MainViewModel>()
+            });
+            return services.BuildServiceProvider();
         }
     }
 
--- SingletonSean/SimpleTrader.WPF/ViewModels/MainViewModel.cs
+++ SingletonSean/SimpleTrader.WPF/ViewModels/MainViewModel.cs
@@ -4,9 +4,10 @@ namespace SimpleTrader.WPF.ViewModels
 {
     public class MainViewModel : ViewModelBase
     {
-        public INavigator Navigator { get; set; } = new Navigator();
-        public MainViewModel()
+        public INavigator Navigator { get; set; }
+        public MainViewModel(INavigator navigator)
         {
+            Navigator = navigator;
             Navigator.UpdateCurrentViewModelCommand.Execute(ViewType.Home);
         }
     }
