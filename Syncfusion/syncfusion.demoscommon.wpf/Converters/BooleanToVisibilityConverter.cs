7cb3cac3b2a0fed37f08ca6e0f11952ee7c538c5
tricdt <tricdt@gmail.com>
2024-09-04 14:31:24 +0700

    [Syncfusion.DemoCommons.WPF] add some converters

---
+++ Syncfusion/syncfusion.demoscommon.wpf/Converters/BooleanToVisibilityConverter.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace syncfusion.demoscommon.wpf.Converters
+{
+    /// <summary>
+    /// Convert between boolean and visibility
+    /// </summary>
+    public class BooleanToVisibilityConverter : BoolToObjectConverter
+    {
+        public BooleanToVisibilityConverter()
+        {
+            TrueValue = Visibility.Visible;
+            FalseValue = Visibility.Collapsed;
+        }
+    }
+}
---
+++ Syncfusion/syncfusion.demoscommon.wpf/Converters/VisibilityToBoolConverter.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace syncfusion.demoscommon.wpf.Converters
+{
+    /// <summary>
+    /// This class converts a Visibility enumeration to a boolean value.
+    /// </summary>
+    public class VisibilityToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is Visibility visibility && visibility == Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (value is bool bl && bl) ? Visibility.Visible : Visibility.Collapsed;
+        }
+    }
+}
