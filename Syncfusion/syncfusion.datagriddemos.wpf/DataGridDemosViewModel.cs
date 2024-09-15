18d93ee1f69cabe8a44a1e19bb5fbd9cb3eb6ed9
Tri Nguyen <tricdt@gmail.com>
2024-09-15 19:02:24 +0700

    [Syncfusion.DatagridDemos.WPF] SummariesDemo

--- Syncfusion/syncfusion.datagriddemos.wpf/DataGridDemosViewModel.cs
+++ Syncfusion/syncfusion.datagriddemos.wpf/DataGridDemosViewModel.cs
@@ -57,6 +57,11 @@ namespace syncfusion.datagriddemos.wpf
             groupingDocumentation.Add(new Documentation { Content = "DataGrid - AllowFrozenGroupHeaders API Reference", Uri = new Uri("https://help.syncfusion.com/cr/wpf/Syncfusion.UI.Xaml.Grid.SfDataGrid.html#Syncfusion_UI_Xaml_Grid_SfDataGrid_AllowFrozenGroupHeaders") });
             groupingDocumentation.Add(new Documentation { Content = "DataGrid - Grouping Documentation", Uri = new Uri("https://help.syncfusion.com/wpf/datagrid/grouping") });
             this.Demos.Add(new DemoInfo() { SampleName = "Grouping", GroupName = "DATA PRESENTATION", Description = "This sample showcases the grouping capabilities of data in SfDataGrid. The SfDataGrid enables you to grouping the data by one or more columns.", DemoViewType = typeof(GroupingDemo), Documentations = groupingDocumentation });
+
+            List<Documentation> summaryDocumentation = new List<Documentation>();
+            summaryDocumentation.Add(new Documentation { Content = "Summaries Documentation", Uri = new Uri("https://help.syncfusion.com/wpf/datagrid/summaries") });
+            this.Demos.Add(new DemoInfo() { SampleName = "Summaries", GroupName = "DATA PRESENTATION", Description = "This sample showcases the summary calculation capabilities of data in SfDataGrid. SfDataGrid provides three kinds of summary row collections, namely GroupSummaryRows, TableSummaryRows, and CaptionSummaryRows.", DemoViewType = typeof(SummariesDemo), Documentations = summaryDocumentation });
+
         }
     }
 }
