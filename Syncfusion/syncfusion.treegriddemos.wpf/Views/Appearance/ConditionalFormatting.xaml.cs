9112598d77025bb2226e1e29d8ed1d4efadb3fd1
Tri Nguyen <tricdt@gmail.com>
2024-11-04 15:18:41 +0700

    [Syncfusion.TreegridDemos.WPF] ConditionalFormatingDemo

---
+++ Syncfusion/syncfusion.treegriddemos.wpf/Views/Appearance/ConditionalFormatting.xaml.cs
@@ -0,0 +1,38 @@
+﻿using syncfusion.demoscommon.wpf;
+
+namespace syncfusion.treegriddemos.wpf
+{
+    /// <summary>
+    /// Interaction logic for ConditionalFormatting.xaml
+    /// </summary>
+    public partial class ConditionalFormatting : DemoControl
+    {
+        public ConditionalFormatting()
+        {
+            InitializeComponent();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (this.treeGrid != null)
+            {
+                this.treeGrid.Dispose();
+                this.treeGrid = null;
+            }
+
+            if (this.DataContext != null)
+            {
+                var dataContext = this.DataContext as EmployeeInfoViewModel;
+                dataContext.Dispose();
+                this.DataContext = null;
+            }
+
+            if (this.Resources["StyleConverter"] != null)
+                (this.Resources["StyleConverter"] as CellStyleConverter).cellStyleDemo = null;
+
+            this.Resources.Clear();
+
+            base.Dispose(disposing);
+        }
+    }
+}
