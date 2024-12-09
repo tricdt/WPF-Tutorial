1ae88b40f2b627b1a65d371b1eb5a576170e8e6c
Tri Nguyen - LAPTOP <tricdt@gmail.com>
2024-12-09 14:03:12 +0700

    [Syncfusion.LedSign.WPF] LedSign

---
+++ Syncfusion/showcase/syncfusion.ledsign.wpf/ViewModels/ViewModelLocator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace syncfusion.ledsign.wpf
+{
+    public class ViewModelLocator
+    {
+        private LocationViewModel locationViewModel;
+
+        public LocationViewModel LocationViewModel
+        {
+            get {
+                if (locationViewModel == null) locationViewModel = new LocationViewModel();
+                return locationViewModel;
+            }
+            set { locationViewModel = value; }
+        }
+
+    }
+}
