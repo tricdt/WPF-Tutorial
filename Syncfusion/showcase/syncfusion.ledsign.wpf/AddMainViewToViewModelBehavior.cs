1555c4228ae4deede27fd167da543e13dc161795
Nguyễn Trần Hồng Trí <tricdt@gmail.com>
2024-12-11 09:25:27 +0700

    [Syncfusion.Ledsign.WPF] ledsign

---
+++ Syncfusion/showcase/syncfusion.ledsign.wpf/AddMainViewToViewModelBehavior.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xaml.Behaviors;
+using Syncfusion.Windows.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace syncfusion.ledsign.wpf
+{
+    public class AddMainViewToViewModelBehavior : Behavior<MainWindow>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            if(this.AssociatedObject.DataContext is ViewModelLocator)
+                (this.AssociatedObject.DataContext as ViewModelLocator).LocationViewModel.MainWindow = this.AssociatedObject as MainWindow;
+        }
+    }
+}
---
+++ Syncfusion/showcase/syncfusion.ledsign.wpf/Helpers/SampleGrid.cs
@@ -0,0 +1,72 @@
+﻿using Syncfusion.Windows.Controls.Grid;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows;
+using Syncfusion.Windows.Controls.Cells;
+
+namespace syncfusion.ledsign.wpf
+{
+    public class SampleGrid : GridControl
+    {
+        public SampleGrid()
+        {
+            Model.RowCount = 10;
+            Model.ColumnCount = 7;
+            Model.ColumnWidths.DefaultLineSize = 25;
+            Model.RowHeights.DefaultLineSize = 25;
+            Model.ColumnWidths[0] = 30d;
+            Model.ColumnWidths[1] = 40d;
+
+            Model.CellModels.Add("LedEdit", new LedEditCellModel());
+            Model.TableStyle.HorizontalAlignment = HorizontalAlignment.Center;
+            Model.TableStyle.VerticalAlignment = VerticalAlignment.Center;
+            AllowDragDrop = false;
+            Model.TableStyle.Borders = new CellBordersInfo()
+            {
+                All = new Pen(Brushes.Gray, 0.05d)
+            };
+
+            Model.CoveredRanges.Add(new CoveredCellInfo(0, 0, 0, 2));
+
+            for (int i = 1; i < Model.RowCount; i++)
+            {
+                Model[i, 0].CellType = "Header";
+                Model[i, 0].CellValue = i;
+                Model[i, 1].CellValue = 200;
+                Model[i, 2].CellValue = 1;
+            }
+            for (int i = 3; i < Model.ColumnCount; i++)
+            {
+                Model[0, i].CellType = "Header";
+                Model[0, i].CellValue = i - 3 + 1;
+                for (int j = 1; j < Model.RowCount; j++)
+                {
+                    Model[j, i].CellType = "LedEdit";
+                    Model[j, i].CellValue = (i - 3) % 16;
+                }
+            }
+        }
+        protected override void OnRender(DrawingContext dc)
+        {
+            base.OnRender(dc);
+        }
+    }
+
+    public class LedEditCellModel : GridCellModel<LedEditCellRenderer>
+    {
+
+    }
+
+    public class LedEditCellRenderer : GridCellIntegerEditCellRenderer
+    {
+        protected override void OnRender(DrawingContext dc, RenderCellArgs rca, GridRenderStyleInfo style)
+        {
+            SampleGrid grid = GridControl as SampleGrid;
+            base.OnRender(dc, rca, style);
+        }
+    }
+}
