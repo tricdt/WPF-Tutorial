237c91b19a2414b2b12381a8343db2f27e1f6ded
Nguyễn Trần Hồng Trí <tricdt@gmail.com>
2025-01-08 10:00:33 +0700

    [DotnetSample] ComponentResourceKey

---
+++ Dotnet-desktop-guide/ComponentResourceKey/Resources/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Resources
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void DynDemoButtonChangeInstanceStat_Click(object sender, RoutedEventArgs e)
+        {
+            DynamicDemos.Resources["BoundAsStaticResource"] = new MyCustomClass() { StringProperty = "BoundAsStaticResourceV2", IntegerProperty = 1 };
+        }
+        private void DynDemoButtonChangeInstanceDyn_Click(object sender, RoutedEventArgs e)
+        {
+            DynamicDemos.Resources["BoundAsDynamicResource"] = new MyCustomClass() { StringProperty = "BoundAsDynamicResourceV2", IntegerProperty = 1 };
+        }
+        private void DynDemoButtonChangeProperty_Click(object sender, RoutedEventArgs e)
+        {
+            var currentInstance = DynamicDemos.Resources["BoundAsDynamicResource"] as MyCustomClass;
+            currentInstance.StringProperty = "BoundAsDynamicResourceChangePropertyV3";
+        }
+    }
+}
---
+++ Dotnet-desktop-guide/ComponentResourceKey/Resources/MyCustomClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resources
+{
+    public class MyCustomClass
+    {
+        public string StringProperty { get; set; }
+        public int IntegerProperty { get; set; }
+        public override string ToString()
+        {
+            return "This MyCustomClass instance\n has a value for StringProperty of [" + StringProperty + "] \n and a value of IntegerProperty of [" + IntegerProperty + "]";
+        }
+    }
+}
