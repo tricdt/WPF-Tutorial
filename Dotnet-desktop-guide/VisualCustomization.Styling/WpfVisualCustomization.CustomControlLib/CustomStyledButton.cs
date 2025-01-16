5e5ec8220d2d35321083ac90ec24c52e7f82e662
Tri Nguyen - LAPTOP <tricdt@gmail.com>
2025-01-16 19:51:37 +0700

    [DotnetSample] VisualCustomization.Styling

---
+++ Dotnet-desktop-guide/VisualCustomization.Styling/WpfVisualCustomization.CustomControlLib/CustomStyledButton.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfVisualCustomization.CustomControlLib
+{
+    public class CustomStyledButton : Button
+    {
+        static CustomStyledButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CustomStyledButton), new FrameworkPropertyMetadata(typeof(CustomStyledButton)));
+        }
+    }
+}
