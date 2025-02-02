679d4b96cd625f002a82698386cbff3b4eaaaa61
Tri Nguyen - LAPTOP <tricdt@gmail.com>
2025-02-02 14:59:32 +0700

    [DotnetSample] VisualCustomization.Triggers

---
+++ Dotnet-desktop-guide/VisualCustomization.Triggers/VisualCustomization.Triggers/MyCustomMultiValueConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace VisualCustomization.Triggers
+{
+    public class MyCustomMultiValueConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            return "V1:" + values[0].ToString() + ";V2:" + values[1].ToString();
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException("Cannot convert back");
+        }
+    }
+}
