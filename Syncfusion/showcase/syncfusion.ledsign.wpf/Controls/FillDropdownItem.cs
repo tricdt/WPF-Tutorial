08574f8f3d15caec0ea3c7d339ff2b04dac9e72d
Nguyễn Trần Hồng Trí <tricdt@gmail.com>
2025-02-19 15:48:25 +0700

    [Syncfusion.Ledsign.WPF] FillDropdownItem

--- Syncfusion/showcase/syncfusion.ledsign.wpf/Controls/FillDropdownItem.cs
+++ Syncfusion/showcase/syncfusion.ledsign.wpf/Controls/FillDropdownItem.cs
@@ -1,16 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Syncfusion.Windows.Tools.Controls;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System;
+using Syncfusion.SfSkinManager;
 
 namespace syncfusion.ledsign.wpf
 {
     public class FillDropdownItem : Control
     {
+        public static readonly DependencyProperty ItemsSourceProperty = DependencyProperty.Register("ItemsSource", typeof(ObservableCollection<RibbonRadioButton>), typeof(FillDropdownItem), new PropertyMetadata(null));
+        public static readonly DependencyProperty SelectedItemProperty = DependencyProperty.Register("SelectedItem", typeof(RibbonRadioButton), typeof(FillDropdownItem), new PropertyMetadata(null, OnSelectedItemChangedCallback));
+
+
+
         public FillDropdownItem()
         {
             this.DefaultStyleKey = typeof(FillDropdownItem);
@@ -18,24 +21,53 @@ namespace syncfusion.ledsign.wpf
 
         public FillDropdownItem(LedEditMarkerMouseController SeriesController) : this()
         {
-            ItemsSource = new ObservableCollection<string> { "Copy", "Cut" };
+            ItemsSource = new ObservableCollection<RibbonRadioButton>();
+            if((SeriesController.Filltype & SeriesType.CopySeries) == SeriesType.CopySeries)
+            {
+                RibbonRadioButton CopySeriesButton = new RibbonRadioButton() { Content = "CopySeries", GroupName = "FillSeriesGroup" };
+
+                ItemsSource.Add(CopySeriesButton);
+            }
+
+            if ((SeriesController.Filltype & SeriesType.FillSeries) == SeriesType.FillSeries)
+            {
+                RibbonRadioButton CopySeriesButton = new RibbonRadioButton() { Content = "FillSeries", GroupName = "FillSeriesGroup" };
+
+                ItemsSource.Add(CopySeriesButton);
+            }
+
+            if ((SeriesController.Filltype & SeriesType.FillFormatOnly) == SeriesType.FillFormatOnly)
+            {
+                RibbonRadioButton CopySeriesButton = new RibbonRadioButton() { Content = "FillFormatOnly", GroupName = "FillSeriesGroup" };
+
+                ItemsSource.Add(CopySeriesButton);
+            }
+
+            if ((SeriesController.Filltype & SeriesType.FillWithoutFormat) == SeriesType.FillWithoutFormat)
+            {
+                RibbonRadioButton CopySeriesButton = new RibbonRadioButton() { Content = "FillWithoutFormat", GroupName = "FillSeriesGroup" };
+
+                ItemsSource.Add(CopySeriesButton);
+            }
         }
 
 
-
-
-        public ObservableCollection<string> ItemsSource
+        public RibbonRadioButton SelectedItem
         {
-            get { return (ObservableCollection<string>)GetValue(ItemsSourceProperty); }
+            get { return (RibbonRadioButton)GetValue(SelectedItemProperty); }
+            set { SetValue(SelectedItemProperty, value); }
+        }
+
+        public ObservableCollection<RibbonRadioButton> ItemsSource
+        {
+            get { return (ObservableCollection<RibbonRadioButton>)GetValue(ItemsSourceProperty); }
             set { SetValue(ItemsSourceProperty, value); }
         }
 
-        // Using a DependencyProperty as the backing store for ItemsSource.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty ItemsSourceProperty =
-            DependencyProperty.Register("ItemsSource", typeof(ObservableCollection<string>), typeof(FillDropdownItem), new PropertyMetadata(null));
-
-
-
+        private static void OnSelectedItemChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            (e.NewValue as RibbonRadioButton).IsChecked = true;
+        }
         public override void OnApplyTemplate()
         {
             base.OnApplyTemplate();
